8dcaa19b441c2c1fcd15a72ef07d3782975be949
Dom <dompetriella@gmail.com>
2022-09-22 23:29:00 -0400

    added animations might roll back for performance issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/main_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const ProviderScope(child: DiceRoller()));
