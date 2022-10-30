939bcfaa7a556983c5a70c01fc304a119ed5d362
Dominic Petriella <dpetriella@magicwrighter.com>
2022-10-30 16:59:38 -0400

    changed stats column to stack for consistency

--- lib/components/drawer/stats.dart
+++ lib/components/drawer/stats.dart
@@ -13,70 +13,72 @@ class StatsBar extends ConsumerWidget {
       flex: 2,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Container(
-          decoration: BoxDecoration(
-              color: ref.watch(themeProvider).drawerColumnBg,
-              borderRadius: BorderRadius.all(
-                  Radius.circular(ref.watch(themeProvider).drawerBorderRadius)),
-              boxShadow: [ref.watch(themeProvider).innerShadow]),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Stats',
-                        style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.height * 0.03,
-                            fontWeight: FontWeight.w900,
-                            color:
-                                ref.watch(themeProvider).drawerColumnTextColor),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 7,
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 8),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: ref.watch(themeProvider).drawerColumnBg,
+                  borderRadius: BorderRadius.all(Radius.circular(
+                      ref.watch(themeProvider).drawerBorderRadius)),
+                  boxShadow: [ref.watch(themeProvider).innerShadow]),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 3,
                     child: Container(
-                      decoration: BoxDecoration(
-                          boxShadow: [
-                            ref.watch(themeProvider).columnShadow,
-                            ref.watch(themeProvider).diceButtonOutline
-                          ],
-                          borderRadius: BorderRadius.all(Radius.circular(
-                              ref.watch(themeProvider).themeBarBorderRadius)),
-                          color:
-                              ref.watch(themeProvider).drawerColumnContentBg),
-                      child: Padding(
-                          padding: const EdgeInsets.fromLTRB(2, 5, 2, 5),
-                          child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: const [
-                                StatsLog(
-                                  diceValue: '20',
-                                ),
-                                StatsLog(diceValue: '6'),
-                                StatsLog(diceValue: '4'),
-                                StatsLog(diceValue: '8'),
-                                StatsLog(diceValue: '10'),
-                                StatsLog(diceValue: '12'),
-                                StatsLog(diceValue: '100')
-                              ])),
+                      padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 8),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                ref.watch(themeProvider).columnShadow,
+                                ref.watch(themeProvider).diceButtonOutline
+                              ],
+                              borderRadius: BorderRadius.all(Radius.circular(ref
+                                  .watch(themeProvider)
+                                  .themeBarBorderRadius)),
+                              color: ref
+                                  .watch(themeProvider)
+                                  .drawerColumnContentBg),
+                          child: Padding(
+                              padding: const EdgeInsets.fromLTRB(2, 5, 2, 5),
+                              child: ListView(
+                                  scrollDirection: Axis.horizontal,
+                                  children: const [
+                                    StatsLog(
+                                      diceValue: '20',
+                                    ),
+                                    StatsLog(diceValue: '6'),
+                                    StatsLog(diceValue: '4'),
+                                    StatsLog(diceValue: '8'),
+                                    StatsLog(diceValue: '10'),
+                                    StatsLog(diceValue: '12'),
+                                    StatsLog(diceValue: '100')
+                                  ])),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                'Stats',
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height * 0.03,
+                    fontWeight: FontWeight.w900,
+                    color: ref.watch(themeProvider).drawerColumnTextColor),
+              ),
+            ),
+          ],
         ),
       ),
     );
